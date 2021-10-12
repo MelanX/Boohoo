5861e403d16cf77b776b94eb132b1101322aa710
MelanX <info@melanx.de>
2021-10-12 18:27:16 +0200

    fix ghost deletes all items

--- src/main/java/de/melanx/boohoo/ghost/Ghost.java
+++ src/main/java/de/melanx/boohoo/ghost/Ghost.java
@@ -26,6 +26,7 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
@@ -86,7 +87,7 @@ public class Ghost extends Monster {
 
         // drop inventory and remove ghost from world
         if (this.vanishCounter <= 0) {
-            this.disappear();
+            this.kill();
         }
 
         // count downwards
@@ -134,7 +135,7 @@ public class Ghost extends Monster {
 
             // player dead -> take one item, fly away and activate vanish counter
             if (!player.isAlive()) {
-                if (ModConfig.stealItems) {
+                if (ModConfig.stealItems && this.level.getGameRules().getBoolean(GameRules.RULE_MOBGRIEFING) && this.getItemInHand(InteractionHand.MAIN_HAND).isEmpty()) {
                     AABB bb = player.getBoundingBox().deflate(3, 3, 3);
                     List<ItemEntity> itemEntities = this.level.getEntitiesOfClass(ItemEntity.class, bb);
                     Optional<ItemEntity> item = itemEntities.stream().findAny();
